3492cd2bb5fc666f3b49bfa0b1a5ab3020f80251
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-10-19 21:22:15 +0900

    test5

--- index.ts
+++ index.ts
@@ -1,26 +1,18 @@
+const handler = (req: Request): Response => {
+  return new Response("Hello, World!", {
+    status: 200,
+    headers: { "Content-Type": "text/plain" },
+  });
+};
+
 const server = Deno.listen({ port: 8080 });
 console.log("Listening on http://localhost:8080/");
 
 for await (const conn of server) {
   (async () => {
-    const body = `
-      <!DOCTYPE html>
-      <html lang="ja">
-      <head>
-        <meta charset="UTF-8">
-        <title>Hello</title>
-      </head>
-      <body>
-        <h1>HELLO</h1>
-      </body>
-      </html>
-    `;
-
-    await Deno.stdout.write(new TextEncoder().encode("HTTP/1.1 200 OK\r\n"));
-    await Deno.stdout.write(new TextEncoder().encode(`Content-Length: ${body.length}\r\n`));
-    await Deno.stdout.write(new TextEncoder().encode(`Content-Type: text/html\r\n\r\n`));
-    await Deno.stdout.write(new TextEncoder().encode(body));
-    
+    const httpConn = Deno.upgradeWebSocket(conn);
+    const response = handler(httpConn);
+    await Deno.writeAll(conn, new TextEncoder().encode(await response.text()));
     conn.close();
   })();
 }
